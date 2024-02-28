9a9c42ce61c1566e2e32e05dd601e6dd50408987
Mohamed Sobh <mohamed.alhusieny@gmail.com>
2024-02-28 20:30:29 +0200

    Enabled mouse keys for scrolling.

--- keyboards/ergodox_ez/keymaps/hrm/rules.mk
+++ keyboards/ergodox_ez/keymaps/hrm/rules.mk
@@ -1,11 +1,13 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
+ORYX_ENABLE = yes
+
+# QMK
+# TAP_DANCE_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+MOUSEKEY_ENABLE = yes
 CONSOLE_ENABLE = no
 COMMAND_ENABLE = no
-MOUSEKEY_ENABLE = no
-ORYX_ENABLE = yes
-#TAP_DANCE_ENABLE = yes
-CAPS_WORD_ENABLE = yes
 LTO_ENABLE = yes
 
 # https://docs.qmk.fm/#/custom_quantum_functions?id=deferred-execution
